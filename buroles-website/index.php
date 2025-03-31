3e72aeb6d005c2f37e27d2b26c415bec6a707a4e
Lex C <123858280+aLexazami@users.noreply.github.com>
2025-04-01 05:02:21 +0800

    Add files via upload

---
+++ buroles-website/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>BES Feedbacking Platform</title>
+</head>
+<body>
+  <header>
+
+  </header>
+
+  <section>
+    <form action="login.php" method="POST">
+      <label for="username">Username:</label>
+      <input type="text" id="username" name="username" required>
+      
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" required>
+      
+      <button type="submit">Login</button>
+    </form>
+  </section>
+  <footer>
+
+  </footer>
+</body>
+</html>
+<?php
+
+?>
+
